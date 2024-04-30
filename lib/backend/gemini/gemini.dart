c295209a9203111a8c855ec7d6187bbbaa5ae419
ABHISHEK KAUSHIK <107637702+abhishek09827@users.noreply.github.com>
2024-04-30 19:50:44 +0530

    Update gemini.dart

--- lib/backend/gemini/gemini.dart
+++ lib/backend/gemini/gemini.dart
@@ -3,7 +3,7 @@ import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:http/http.dart' as http;
 import '/flutter_flow/flutter_flow_util.dart';
 
-const _kGemeniApiKey = 'AIzaSyAVSHjy7mpfwjsStcK9YassCIL9BZOPKkA';
+const _kGemeniApiKey = 'GoogleAPIKey';
 
 Future<String?> geminiGenerateText(
   BuildContext context,
